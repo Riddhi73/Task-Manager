d3c31100da7586c8c65051bb2be3c070e94dabe1
Riddhi73 <riddhimandebnath4260@gmail.com>
2026-06-12 10:48:09 +0600

    Define task state structure

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
 
 export default function TaskBoard() {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React",
+    description: "Learn React within 3 week",
+    tag: ["web", "react", "API"],
+    priority: "High",
+    isFavourite: true,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
+
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
@@ -11,7 +22,7 @@ export default function TaskBoard() {
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskActions />
-          <TaskList />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
